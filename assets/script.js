faa5000cddd046361672993c342d56514fe4d2f8
Arpita Pal <apal59349@gmail.com>
2025-07-17 15:27:09 +0530

    first commit

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    578: {
+      items: 2,
+    },
+    769: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
