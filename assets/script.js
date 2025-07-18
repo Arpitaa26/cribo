0268afc6da04f45627b1f67fc397406e2c44f7e3
Arpita Pal <apal59349@gmail.com>
2025-07-18 09:56:13 +0530

    changes

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,31 @@
 $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
-  nav: true,
+  nav: false,
+  dots: true,
   responsive: {
     0: {
       items: 1,
     },
-    578: {
-      items: 2,
-    },
-    769: {
+    600: {
       items: 3,
     },
     1000: {
       items: 5,
     },
   },
+});
+
+// mobile menu //////////////////////
+
+$("#mobile-menu-btn").click(function () {
+  $(".mobile-menu").addClass("show");
+});
+
+$("#mobile-close-btn").click(function () {
+  $(".mobile-menu").removeClass("show");
+});
+
+$(".mobile-menu-close").click(function () {
+  $(".mobile-menu").removeClass("show");
 });
