4af5cbebc9d47005956b06394b6f7ddff3cd1eb3
Arpita Pal <arpita.pal@consectus.com>
2025-08-02 08:31:52 +0530

    add main logo image to assets

--- assets/script.js
+++ assets/script.js
@@ -18,14 +18,28 @@ $(".owl-carousel").owlCarousel({
 
 // mobile menu //////////////////////
 
-$("#mobile-menu-btn").click(function () {
-  $(".mobile-menu").addClass("show");
-});
+// $("#mobile-menu-btn").click(function () {
+//   $(".mobile-menu").addClass("show");
+// });
 
-$("#mobile-close-btn").click(function () {
-  $(".mobile-menu").removeClass("show");
-});
+// $("#mobile-close-btn").click(function () {
+//   $(".mobile-menu").removeClass("show");
+// });
 
-$(".mobile-menu-close").click(function () {
-  $(".mobile-menu").removeClass("show");
+// $(".mobile-menu-close").click(function () {
+//   $(".mobile-menu").removeClass("show");
+// });
+   const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+    const navActions = document.getElementById('nav-actions');
+
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+      navActions.classList.toggle('active');
+    });
+    const closeBtn = document.getElementById('mobile-close-btn');
+
+closeBtn.addEventListener('click', () => {
+  navLinks.classList.remove('active');
+  navActions.classList.remove('active');
 });
